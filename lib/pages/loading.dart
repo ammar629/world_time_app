2972d78946c437fb211518a93ad936c17aed93a4
Ammar <33799463+ammar629@users.noreply.github.com>
2023-09-21 20:11:52 +0500

    Http request function implemented
    * Added the getData() function to fetch data from remote endpoint
    * Temporarily switched route to '/' for testing purposes

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,10 +11,17 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  void getData() async{
+    Response data = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map jsonDecodedData = jsonDecode(data.body);
+
+    print(jsonDecodedData["title"]);
+  }
+
   @override
   void initState(){
     super.initState();
-
+    getData();
   }
 
   @override
