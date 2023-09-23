f7213643935497ae5eb66dc716a1df6ba8759190
Ammar <33799463+ammar629@users.noreply.github.com>
2023-09-23 19:16:29 +0500

    feat, fix: Add asset, time of day check
    - Updated pubspec.yaml to include the assets/ folder
    - Added isDayTime boolean to check if it is day time or not (Between 6 AM and 8 PM)
    - Display proper background in accordance with time of day (day/night)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,44 +15,56 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context)!.settings.arguments as Map;
 
     print(data);
+
+    // Set Background
+    String bgImage = data["isDayTime"] ? "day.png" : "night.png";
+
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.fromLTRB(0.0, 120, 0, 0),
-        child: Column(
-          children: [
-            TextButton.icon(
-              onPressed: (() {
-                Navigator.pushNamed(context, '/location');
-              }),
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgImage'),
+                fit: BoxFit.cover,
+                ),
             ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  data['location'],
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    letterSpacing: 2.0,
+            child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 120, 0, 0),
+                  child: Column(
+            children: [
+              TextButton.icon(
+                onPressed: (() {
+                  Navigator.pushNamed(context, '/location');
+                }),
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location'),
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 66.0,
+                ),
+              ),
+            ],
                   ),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              data['time'],
-              style: TextStyle(
-                fontSize: 66.0,
-              ),
-            ),
-          ],
-        ),
-      )),
+          )),
     );
   }
 }
