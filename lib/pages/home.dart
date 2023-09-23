c0793fece7010641b0fa957712b929e620e402ff
Ammar <33799463+ammar629@users.noreply.github.com>
2023-09-23 19:35:54 +0500

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,6 +42,7 @@ class _HomeState extends State<Home> {
                   Icons.edit_location,
                   color: Colors.grey[300],
                   ),
+                label: Text(
                   'Edit Location',
                   style: TextStyle(
                     color: Colors.grey[300],
